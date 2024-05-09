90c5dddc65632a2d5b9122ec075a1ea2eb882121
Tadeja-S <161042086+Tadeja-S@users.noreply.github.com>
2024-05-09 10:18:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,10 +8,40 @@ def index():
 
 @app.route("/convert", methods=["POST"])
 def convert():
-    km = float(request.form.get("km"))
-    miles = km * 0.62
+    value = request.form.get("unit")
+    input = float(request.form.get("number"))
 
-    return render_template("result.html", km=km, miles=miles) # Templatu sporočimo, katere spremenljivke bomo uporabili na tej html strani!
+    if value == "km to miles":
+        miles = input * 0.621371
+        return render_template("result.html", value=value, input=input, result=miles)
+    
+    elif value == "miles to km":
+        km = input * 1.60934
+        return render_template("result.html", value=value, input=input, result=km)
+
+    elif value == "C° to F°":
+        F = (input * 9/5) + 32
+        return render_template("result.html", value=value, input=input, result=F)
+
+    elif value == "F° to C°":
+        C = (input - 32) * 5/9
+        return render_template("result.html", value=value, input=input, result=C)
+
+    elif value == "EUR to USD":
+        USD = input * 1.07729
+        return render_template("result.html", value=value, input=input, result=USD)
+
+    elif value == "USD to EUR":
+        EUR = input * 0.928482
+        return render_template("result.html", value=value, input=input, result=EUR)
+    
+    elif value == "kg to lbs":
+        pounds = input * 2.20462
+        return render_template("result.html", value=value, input=input, result=pounds)
+    
+    elif value == "lbs to kg":
+        kilogrammes = input * 0.453592
+        return render_template("result.html", value=value, input=input, result=kilogrammes)
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
